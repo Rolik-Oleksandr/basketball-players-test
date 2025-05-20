04b4256a6158d011bf67dd32ae02c37efd9132a5
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-20 19:24:57 +0300

    Initial Commit

---
+++ basketball-players-test/basketball_players_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  basketball_players_testApp.swift
+//  basketball-players-test
+//
+//  Created by Oleksandr Rolik on 20.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct basketball_players_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
